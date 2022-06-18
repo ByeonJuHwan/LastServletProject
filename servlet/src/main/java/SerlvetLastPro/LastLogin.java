2886781d3ec9ca2a48426a1a7e703f7cd748f3ec
byeonjuhwan <byeonjuhwan@localhost>
2022-06-19 02:08:49 +0900

    첫 커

---
+++ servlet/src/main/java/SerlvetLastPro/LastLogin.java
@@ -0,0 +1,23 @@
+package SerlvetLastPro;
+
+import jakarta.servlet.http.HttpServlet;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/lastlogin")
+public class LastLogin extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
