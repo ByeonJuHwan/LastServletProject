b5e8dcffc2f701eb8b7b2c32b3e1ed7410d0933c
byeonjuhwan <byeonjuhwan@localhost>
2022-06-28 21:38:28 +0900

    로그인창에서 관리자, 일반사용자 나누기

--- LastServlet/src/main/java/lastServlet/LastLogin.java
+++ LastServlet/src/main/java/lastServlet/LastLogin.java
@@ -22,6 +22,21 @@ public class LastLogin extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
+		String id = request.getParameter("id");
+		String pwd = request.getParameter("pwd");
+		if((id!=null&&id.equals("byeon"))&&(pwd!=null&&pwd.equals("0111"))) {
+			out.println("<html><body>");
+			out.println("<h1>관리자님 환영합니다!!</h1>");
+			out.println("<form method='get' action='admin'>");
+			out.println("<br><br>");
+			out.println("<button type='submit' value='회원관리'>");
+			out.println("<input type='hidden' name='cancle' value='delMember'/>");
+			out.println("회원관리</button>");
+			out.println("</body></html>");
+			
+		}else {
+			out.print("<h1>"+id+"님 환영합니다!!</h1>");
+		}
 		
 	}
 	
